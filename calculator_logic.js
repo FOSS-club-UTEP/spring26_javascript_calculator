e809fc930a284c57bdb6e21762c153054e4fc0d1
godine <godine_0@proton.me>
2026-03-23 11:48:09 -0600

    completed calculator logic and theme toggle logic javascript files for reference

--- calculator_logic.js
+++ calculator_logic.js
@@ -1,1 +1,81 @@
-/* calculator logic here */
+let currExpr = '';
+
+function updateDisplay(value) {
+	const display = document.getElementById('result');
+	display.innerText = value || '0';
+}
+
+window.appendNum = function(number) {
+	if (number == '.' && currExpr.split(/[\+\-\*\/]/).pop().includes('.')) {
+		/* we won't do anything, this way the decimal won't be added */
+		return;
+	}
+	currExpr += number;
+	updateDisplay(currExpr);
+}
+
+window.appendOp = function(operator) {
+	currExpr += `${operator}`;
+	updateDisplay(currExpr);
+}
+
+/* more "correct" way */
+window.calcRes = function() {
+	try {
+		const tokens = currExpr.match(/\d+\.?\d*|[\+\-\*\/]/g);
+
+		if (!tokens) return;
+
+		let values = [];
+
+		for (let i = 0; i < tokens.length; i++) {
+			let token = tokens[i];
+
+			if (token === '*' || token === '/') {
+				let prevNum = parseFloat(values.pop());
+				let nextNum = parseFloat(tokens[++i]);
+				let res = (token === '*') ? prevNum * nextNum : prevNum / nextNum;
+				values.push(res);
+			} else {
+				values.push(token);
+			}
+		}
+
+		let finalResult = parseFloat(values[0]);
+		for (let i = 1; i < values.length; i += 2) {
+			let operator = values[i];
+			let nextNum = parseFloat(values[i + 1]);
+
+			if (operator === '+') finalResult += nextNum;
+			if (operator === '-') finalResult -= nextNum;
+
+		}
+		currExpr = finalResult.toString();
+		updateDisplay(currExpr);
+	} catch (e) {
+		updateDisplay("Error");
+		currExpr = '';
+	}
+}
+
+/* easy way */
+/*
+window.calcRes = function() {
+	try {
+		currExpr = eval(currExpr.replace('÷', '/').replace('×', '*'));
+		updateDisplay(currExpr);
+	} catch {
+		updateDisplay("Error");
+	}
+}
+*/
+
+window.clearRes = function() {
+	currExpr = '';
+	updateDisplay(currExpr);
+}
+
+window.del = function() {
+	currExpr = currExpr.substring(0, currExpr.length - 1);
+	updateDisplay(currExpr);
+}
--- theme_toggle.js
+++ theme_toggle.js
@@ -1,1 +1,21 @@
-/* theme toggle logic here */
+const toggleBtn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+	document.documentElement.setAttribute('data-theme', currentTheme);
+	toggleBtn.textContent = currentTheme === 'dark' ? '☼ Switch to light mode' : '☾ Switch to dark mode';
+}
+
+toggleBtn.addEventListener('click', () => {
+	let theme = document.documentElement.getAttribute('data-theme');
+
+	if (theme === 'dark') {
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+		toggleBtn.textContent = '☾ Switch to dark mode';
+	} else {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+		toggleBtn.textContent = '☼ Switch to light mode';
+	}
+});	
